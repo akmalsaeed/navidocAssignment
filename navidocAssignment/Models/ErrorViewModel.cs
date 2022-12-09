1d6091efaeec408675f533cc1d0ceeaaf941142e
akmal saeed <akmal saeed@DESKTOP-T4LP2JK>
2022-12-09 08:25:42 -0800

    Add project files.

---
+++ navidocAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace navidocAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
